7f5d192e993b3eb648c83815e9c034c659c3ffdd
syedasqa <122955503+syedasqa@users.noreply.github.com>
2023-02-24 20:00:10 -0600

    new changes

--- src/main/java/variousConepts/GitChanges.java
+++ src/main/java/variousConepts/GitChanges.java
@@ -4,7 +4,20 @@ public class GitChanges {
 
 	public static void main(String[] args) {
 		System.out.println("git test");
-	
 	}
+		
+		public void printMe1() {
+			System.out.println("git test 1");
+			}
+		
+		public void printMe2() {
+			System.out.println("git test 2");
+			}
+	
+	
 
+	
+	
+
+	
 }
